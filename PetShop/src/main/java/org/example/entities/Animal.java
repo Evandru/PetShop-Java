b9228627de36eccf3e7cc984453957de515d40fd
Evandru <evandro_remolli.t@outlook.com>
2023-07-30 19:20:48 -0300

    add: Animal abstract class

---
+++ PetShop/src/main/java/org/example/entities/Animal.java
@@ -0,0 +1,75 @@
+package org.example.entities;
+
+import java.util.Objects;
+
+public abstract class Animal {
+    private Long id;
+    private String name;
+    private Integer age;
+    private Double weight;
+
+    public Animal(Long id, String name, Integer age, Double weight) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    private void eat(){
+        System.out.println(name + " está comendo.");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(id, animal.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
