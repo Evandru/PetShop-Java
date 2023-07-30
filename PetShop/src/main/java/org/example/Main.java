b1b20929ff1664acc0afb3652ced0ea50c539f9f
Evandru <evandro_remolli.t@outlook.com>
2023-07-30 13:36:43 -0300

    add: DbConnection Instantiate

--- PetShop/src/main/java/org/example/Main.java
+++ PetShop/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DbConnection dbc = new DbConnection();
+        dbc.connection();
     }
 }
