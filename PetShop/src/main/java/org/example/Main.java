d501459da0f356fd6914ba660bb2287dbc336952
Evandru <evandro_remolli.t@outlook.com>
2023-07-31 13:03:51 -0300

    modify: added try catch and empty preparedStatement

--- PetShop/src/main/java/org/example/Main.java
+++ PetShop/src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         DbConnection dbc = new DbConnection();
-        dbc.connection();
+        try {
+            var db = dbc.connection();
+            PreparedStatement ps = db.prepareStatement("");
+            ResultSet rs = ps.executeQuery();
+            System.out.println(rs);
+            db.close();
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
