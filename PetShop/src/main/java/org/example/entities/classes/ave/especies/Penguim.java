26d3ea64fdc35eff94c66890c6d702e012a38d0a
Evandru <evandro_remolli.t@outlook.com>
2023-07-31 13:41:27 -0300

    add: Penguim class complete

--- PetShop/src/main/java/org/example/entities/classes/ave/especies/Penguim.java
+++ PetShop/src/main/java/org/example/entities/classes/ave/especies/Penguim.java
@@ -1,4 +1,22 @@
 package org.example.entities.classes.ave.especies;
 
-public class Penguim {
+import org.example.entities.classes.ave.Ave;
+
+public class Penguim extends Ave {
+    private String bellyColor;
+
+    public Penguim(Long id, String name, Integer age, Double weight, Double wingWidth, Boolean canFly, String bellyColor) {
+        super(id, name, age, weight, wingWidth, canFly);
+        this.bellyColor = bellyColor;
+    }
+
+    public String getBellyColor() {
+        return bellyColor;
+    }
+
+    public void setBellyColor(String bellyColor) {
+        this.bellyColor = bellyColor;
+    }
+
+
 }
