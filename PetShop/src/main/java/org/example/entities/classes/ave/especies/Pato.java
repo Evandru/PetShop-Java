c10470f769dfb58c968b3d503380938e3f7caf71
Evandru <evandro_remolli.t@outlook.com>
2023-07-31 13:40:29 -0300

    add: Pato class complete

--- PetShop/src/main/java/org/example/entities/classes/ave/especies/Pato.java
+++ PetShop/src/main/java/org/example/entities/classes/ave/especies/Pato.java
@@ -2,6 +2,21 @@ package org.example.entities.classes.ave.especies;
 
 import org.example.entities.classes.ave.Ave;
 
-public class Pato{
+public class Pato extends Ave{
+    private String beakColor;
+
+    public Pato(Long id, String name, Integer age, Double weight, Double wingWidth, Boolean canFly, String beakColor) {
+        super(id, name, age, weight, wingWidth, canFly);
+        this.beakColor = beakColor;
+    }
+
+    public String getBeakColor() {
+        return beakColor;
+    }
+
+    public void setBeakColor(String beakColor) {
+        this.beakColor = beakColor;
+    }
+
 
 }
