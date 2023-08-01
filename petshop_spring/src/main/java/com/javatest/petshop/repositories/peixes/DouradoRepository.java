e44e69c763530ea68a0772c971f07a360e47722e
Evandru <evandro_remolli.t@outlook.com>
2023-08-01 18:21:40 -0300

    add: Dourado repository

---
+++ petshop_spring/src/main/java/com/javatest/petshop/repositories/peixes/DouradoRepository.java
@@ -0,0 +1,8 @@
+package com.javatest.petshop.repositories.peixes;
+
+import com.javatest.petshop.entities.classes.peixe.especies.Dourado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DouradoRepository extends JpaRepository<Dourado, Long> {
+
+}
