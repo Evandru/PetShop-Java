5de8c3cf6f19361aded9d1cf401320b31acf30ac
Evandru <evandro_remolli.t@outlook.com>
2023-08-01 18:21:59 -0300

    add: Tubarao repository

---
+++ petshop_spring/src/main/java/com/javatest/petshop/repositories/peixes/TubaraoRepository.java
@@ -0,0 +1,8 @@
+package com.javatest.petshop.repositories.peixes;
+
+import com.javatest.petshop.entities.classes.peixe.especies.Tubarao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TubaraoRepository extends JpaRepository<Tubarao, Long> {
+
+}
