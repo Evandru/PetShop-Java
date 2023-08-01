374fb6b0776fdc66964e66ed078ee004b114e257
Evandru <evandro_remolli.t@outlook.com>
2023-08-01 17:58:14 -0300

    add: pato subclass

---
+++ petshop_spring/src/main/java/com/javatest/petshop/entities/classes/ave/especies/Pato.java
@@ -0,0 +1,22 @@
+package com.javatest.petshop.entities.classes.ave.especies;
+
+import com.javatest.petshop.entities.classes.ave.Ave;
+
+public class Pato extends Ave{
+    private String beakColor;
+
+    public Pato(Long id, String name, Integer age, Double weight, Double wingWidth, Boolean canFly, String beakColor) {
+        super(id, name, age, weight, wingWidth, canFly);
+        this.beakColor = beakColor;
+    }
+
+    public String getBeakColor() {
+        return beakColor;
+    }
+
+    public void setBeakColor(String beakColor) {
+        this.beakColor = beakColor;
+    }
+
+
+}
