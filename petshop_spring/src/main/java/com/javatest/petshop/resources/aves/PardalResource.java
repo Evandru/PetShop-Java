d79ef3de445b40717da21ff5eb3b773f9bc6cec7
Evandru <evandro_remolli.t@outlook.com>
2023-09-03 11:43:47 -0300

    test: PardalResource request returning Pardal instantiate

--- petshop_spring/src/main/java/com/javatest/petshop/resources/aves/PardalResource.java
+++ petshop_spring/src/main/java/com/javatest/petshop/resources/aves/PardalResource.java
@@ -2,10 +2,16 @@ package com.javatest.petshop.resources.aves;
 
 import com.javatest.petshop.entities.classes.ave.especies.Pardal;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/pardal")
 public class PardalResource {
+    @GetMapping
+    public ResponseEntity<Pardal> findAll(){
+        Pardal p = new Pardal(1L, "name", 1, 1.1, 1.1, true, "color");
+        return ResponseEntity.ok().body(p);
+    }
 }
